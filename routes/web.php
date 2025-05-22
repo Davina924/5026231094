1e561ec6f33813c6f61e0f17818de2decdf3eb6e
DavinaAlmeira <5026231094@student.its.ac.id>
2025-05-22 15:48:15 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 // import java.io ;
 // Route adalah nama kelas
 // System.out.println("Hello World");
@@ -55,3 +58,14 @@ Route::get('index', function () {
 Route::get('frontend', function () {
 	return view('frontend');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang',  [BlogController::class, 'tentang']);
+Route::get('/blog/kontak',  [BlogController::class, 'kontak']);
